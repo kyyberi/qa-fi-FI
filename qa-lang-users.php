747346bc7abca39e41321b7ea80b325335a4e654
Jarkko Moilanen <jarkko.moilanen@ossoil.com>
2013-04-16 18:26:23 +0300

    Fully translated

--- qa-lang-users.php
+++ qa-lang-users.php
@@ -10,7 +10,6 @@
 	Version: See define()s at top of qa-include/qa-base.php
 	Description: Language phrases for user management
 
-
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
 	as published by the Free Software Foundation; either version 2
